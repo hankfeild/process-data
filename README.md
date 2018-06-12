572447ffe0db30a296ade96fc5cd9496a1094b21
Hank Feild <hank@feild.org>
2018-06-12 11:43:18 -0400

    Added sum.cpp.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Hello
 
 myrepo is a collection of C++ programs for processing data files.
+
+## Files
+
+  * sum.cpp -- Sums a column in a specified data file.
---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    // Read in data filename and column to sum.
+    
+    // Read through data file, summing values in given column.
+    
+    // Print out the sum.
+    
+    
+    return 0;
+}
