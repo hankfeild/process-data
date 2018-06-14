646317811f29341aa72b658ced4d24c08acb0384
Hank Feild <hank@feild.org>
2018-06-14 10:12:36 -0700

    Added product.cpp.

---
+++ product.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    // Read in data filename and column to multiply.
+    
+    // Read through data file, multiplying values in given column.
+    
+    // Print out the product.
+    
+    
+    return 0;
+}
+
+float getColumnValueAsFloat(string line, int column){
+    return 0;
+}
+
+float product(string line, int column, float currentProduct){
+	return getColumnValueAsFloat(line, column) * currentSum;
+}
